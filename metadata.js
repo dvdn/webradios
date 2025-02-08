c64b29b2d7398f53aa510dd5c02e60178b0781a3
dvdn <dvdngrllo@gmx.com>
2025-02-08 08:34:55 +0100

    Fix js, unknown broadcast (#27)
    * Fix js, unknown broadcast
    
    * Data, clear unknown broadcast

--- metadata.js
+++ metadata.js
@@ -1,30 +1,26 @@
 function getPathMetadata(radioItem) {
     streamUrl = radioItem.url;
-    streamCast = radioItem.broadcast;
-    console.log(streamCast);
+    streamCast = radioItem.broadcast ? radioItem.broadcast : null;
     switch (streamCast) {
         case 'icecast':
-        var b = streamUrl.lastIndexOf('/');
-        a= streamUrl.substring(0, b) + '/status.xsl';
-        break;
+            var b = streamUrl.lastIndexOf('/');
+            a = streamUrl.substring(0, b) + '/status.xsl';
+            break;
         case 'shoutcast':
-        a = streamUrl;
-        var b = streamUrl.lastIndexOf('/');
-        a= streamUrl.substring(0, b) + '/7.html';
-        break;
-        case '':
-        a = null;
-        break;
+            a = streamUrl;
+            var b = streamUrl.lastIndexOf('/');
+            a = streamUrl.substring(0, b) + '/7.html';
+            break;
         default:
-        a = streamCast;
+            a = streamCast;
     }
     return a;
 }
 
-function displayMetadata(url){
-    str ="";
+function displayMetadata(url) {
+    str = "";
     if (url !== null) {
-        str = "<a href='"+url+"' target=\"_blank\">&nbsp;Quel titre en ce moment ?&nbsp;</a>"
+        str = "<a href='" + url + "' target=\"_blank\">&nbsp;Quel titre en ce moment ?&nbsp;</a>"
     }
-    document.getElementById('webradio-metadata').innerHTML=str;
+    document.getElementById('webradio-metadata').innerHTML = str;
 }
